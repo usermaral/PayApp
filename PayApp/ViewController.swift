121f79779cd9f7ab157ef2a3737edf86e34e565f
maraluser <marallsharipkyzy@gmail.com>
2025-08-08 19:11:41 +0500

    Initial Commit

---
+++ PayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PayApp
+//
+//  Created by Maral Tazebayeva on 08.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
